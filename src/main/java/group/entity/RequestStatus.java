a50f2dc9fcdb605816df38d218a01114cf94655c
Ahmed Mohamed <114872733+GEMIv1@users.noreply.github.com>
2025-05-11 01:42:25 +0300

    modifying files

--- src/main/java/group/entity/RequestStatus.java
+++
@@ -1,5 +0,0 @@
-package group.entity;
-
-public enum RequestStatus {
-    PENDING, APPROVED, REJECTED;
-}
--- src/main/java/user/entity/RequestStatus.java
+++
@@ -1,7 +0,0 @@
-package user.entity;
-
-public enum RequestStatus {
-    PENDING,
-    ACCEPTED,
-    REJECTED;
-}
